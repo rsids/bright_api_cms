a5705f8bd68fc7dc0e71b3f3a33e1e770fc763a4
Ids Klijnsma <werk@idsklijnsma.nl>
2016-10-14 16:55:22 +0200

    Included config

--- gateway.php
+++ gateway.php
@@ -1,10 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ids
- * Date: 14-10-16
- * Time: 16:17
- */
+
+header('Content-Type: text/html; charset=utf-8');
+
+date_default_timezone_set('Europe/Amsterdam');
+
+$configPath = __DIR__ . '/../site/config/Constants.php';
+if(file_exists($configPath)) {
+    require_once $configPath;
+}
 
 require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '/vendor/autoload.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '/vendor/fur/bright/api/amf/index.php';
